3c14221d2c87288632b27cb970da4ccfb3401b08
Cardial <lukasczthegame1234@gmail.com>
2026-08-07 19:13:30 +0200

    Refactor main class to Animals with methods

--- main.java
+++ main.java
@@ -1,8 +1,35 @@
-public class main{
+public class Animals {
+    
+    boolean isAlive;
+    int health;
+    static int animalsCreated;
 
-    public static void main(String[] args){
-
-        System.out.println("Hello world!");
-
+     Animals() {
+        this.isAlive = true;
+        this.health = 100;
+        animalsCreated++;
     }
+
+    void speak(){
+        System.out.println("The animal is speaking \n");
+    }
+
+    void isAlive(){
+        if(isAlive){
+            System.out.print("The animal is alive. \n");
+        } else {
+            System.out.print("The animal is dead. RIP \n");
+        }
+    }
+
+    void eat(){
+        System.out.print("The animal is eating \n");
+    }
+
+    void moving()
+    {
+
+        System.out.print("The animal is moving! \n");
+    }
+
 }
