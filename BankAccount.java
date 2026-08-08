2f57e5b336bc99e3f31af44411724a7cf6128f84
Cardial <lukasczthegame1234@gmail.com>
2026-08-08 03:27:40 +0200

    Bank account -> OOP practice

---
+++ BankAccount.java
@@ -0,0 +1,61 @@
+public class BankAccount {
+
+    private final String accountOwner;
+    private int accountBalance;
+    private boolean isFreezed;
+
+    public BankAccount(String accountOwner, int accountBalance){
+        this.accountOwner = accountOwner;
+        this.accountBalance = accountBalance;
+        this.isFreezed = false;
+    }
+
+    public int showCash(){
+        return this.accountBalance;
+    }
+
+    public String showOwner(){
+        return this.accountOwner;
+    }
+
+    public void addCash(int amount){
+        if(amount < 999999 && amount > 0 && !this.isFreezed){
+            this.accountBalance += amount;
+            System.out.printf("|The amount: %d$ was added to your account| New balance: %d$ \n", amount, this.accountBalance);
+        } else if(this.isFreezed){
+            System.out.println("Your account is freezed. Please contact your bank ASAP.");
+        } else {
+            System.out.println("Problem with adding cash to the account please read rules.");
+        }
+    }
+
+    private void freezeAccount(){
+        if(!this.isFreezed){
+            this.isFreezed = true;
+            System.out.println("Account was succesfully freezed.");
+        } else {
+            System.out.println("The account is already freezed!");
+        }
+
+    }
+
+    public void unfreezeAccount(){
+        if(this.isFreezed){
+            isFreezed = false;
+            System.out.println("Account was successfully unfrozen.");
+        } else {
+            System.out.println("The account is not frozen.");
+        }
+    }
+
+    public void removeCash(int amount){
+        if(this.accountBalance - amount > 0 && !this.isFreezed){
+            this.accountBalance -= amount ;
+            System.out.printf("Cash were succesfully removed from the cash account. New balance: %d$ \n ", this.accountBalance);
+        } else {
+            System.out.println("There was problem with removing cash");
+            freezeAccount();
+        }
+    }
+
+}
--- main.java
+++ main.java
@@ -2,22 +2,12 @@ public class main{
 
     public static void main(){
 
-        Dog dog = new Dog("Chivava");
-        Cat cat = new Cat();
-        Bird bird = new Bird();
-        dog.speak();
-        System.out.print(dog.getRasa() + "\n");
-        cat.eat();
-        cat.prayDetected();
-        System.out.print("LOOKING FOR PREY... \n");
-        cat.preyDissapeared();
+        BankAccount bankAccount = new BankAccount("LukasJava", 3000);
 
-        bird.fly();
-        bird.isAlive();
-        bird.moving();
-
-        System.out.print(Animals.animalsCreated + "\n");
-
+        bankAccount.addCash(300);
+        bankAccount.removeCash(4444);
+        System.out.println(bankAccount.showCash());
+        bankAccount.addCash(22);
 
     }
 }
